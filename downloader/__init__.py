5586feba4e7ffad8031f91aa88edd492fae81ddf
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2015-08-09 13:50:42 +0300

    add __init__.py

---
+++ downloader/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.7
+# coding=utf-8
+"""This is part of [test_blue_yonder](https://github.com/Friz-zy/test_blue_yonder)
+
+License: Apache 2.0
+Copyright (c) 2015 Filipp Kucheryavy aka Frizzy <filipp.s.frizzy@gmail.com>
+
+"""
+
+
+__description__ = 'Download images from the internet'
+__keywords__ = 'download, images, async, blue-yonder.com'
+__url__ = 'https://github.com/Friz-zy/test_blue_yonder'
+__author__ = 'Filipp Frizzy'
+__credits__ = ["Filipp Frizzy"]
+__license__ = "Apache 2.0"
+__maintainer__ = "Filipp Frizzy"
+__email__ = "filipp.s.frizzy@gmail.com"
+__status__ = "Development"
+__version__ = '0.1'
+
+
+from .downloader import *
--- downloader/test_specification.py
+++ downloader/test_specification.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
-"""Requirements specification for the downloader"""
+"""Requirements specification for the downloader
 
-__author__ = 'Filipp Frizzy'
-__credits__ = ["Filipp Frizzy"]
-__license__ = "Apache 2.0"
-__version__ = "0.1"
-__maintainer__ = "Filipp Frizzy"
-__email__ = "filipp.s.frizzy@gmail.com"
-__status__ = "Development"
+This is part of [test_blue_yonder](https://github.com/Friz-zy/test_blue_yonder)
+
+License: Apache 2.0
+Copyright (c) 2015 Filipp Kucheryavy aka Frizzy <filipp.s.frizzy@gmail.com>
+
+"""
 
 
 import os
