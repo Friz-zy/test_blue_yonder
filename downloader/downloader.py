40a496ddad6f4f814d77d48281d5fb5833e66d9b
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2015-08-09 14:56:00 +0300

    add get_file function

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -8,19 +8,56 @@ Copyright (c) 2015 Filipp Kucheryavy aka Frizzy <filipp.s.frizzy@gmail.com>
 """
 
 
+import os
 import sys
+import urllib
+import logging
+
+import gevent
+from gevent import monkey, pool
+monkey.patch_all()
 
 
-def cli(arguments):
+logging.basicConfig(
+    format=u'%(asctime)s  downloader\t%(levelname)-8s\t%(message)s',
+    datefmt='%d %b %Y %H:%M:%S',
+    stream=sys.stdout, # will be replacing by filename
+    #filename= 'downloader.log',
+    #filemode='a',
+    level=logging.INFO,
+)
+
+
+def cli(arguments=sys.argv):
     pass
 
-def get_file(link, output_dir):
+def get_file(link, output_dir=os.getcwd()):
+    """Download file from internet
+
+    Args:
+      link (str): web url
+      output_dir (str): directory for downloading,
+        current dir is default
+
+    Return:
+      0 if success
+      link if fail
+
+    """
+    name = os.path.join(output_dir, link.split('/')[-1])
+    logging.debug('Start downloading: %s as %s', link, name)
+    try:
+        urllib.urlretrieve(url=link, filename=name)
+        logging.info('Finishing downloading: %s as %s', link, name)
+        return 0
+    except Exception as e:
+        logging.error('Can not download %s: %s', link, e)
+        return link
+
+def async_map(function, arguments, pool_size=0):
     pass
 
-def async_map(function, arguments, pool_size):
-    pass
-
-def main(arguments):
+def main(arguments=sys.argv):
     pass
 
 
